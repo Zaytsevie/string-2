4e2665549e71232711257538ba21db70beabd8c3
Zaytsevie <115174587+Zaytsevie@users.noreply.github.com>
2022-10-15 23:48:39 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let myInfoText = "Всем привет! Меня зовут Игорь. Сейчас я изучаю язык программирования JavaScript на курсе по JavaScript у Владилена Минина. Я хочу стать веб-разработчиком, потому что, давно хотел научится создавать сайты. До этого я изучал JavaScript 0 месяцев. Я уверен, что пройду данный курс до конца!"
+console.log(myInfoText.replaceAll('JavaScript', 'javascript'))
+myInfoText = "Всем привет! Меня зовут Игорь. Сейчас я изучаю язык программирования javascript на курсе по javascript у Владилена Минина. Я хочу стать веб-разработчиком, потому что, давно хотел научится создавать сайты. До этого я изучал javascript 0 месяцев. Я уверен, что пройду данный курс до конца!"
+console.log(myInfoText.replaceAll("курс", "КУРС"))
+console.log(myInfoText.length)
+console.log(myInfoText.slice(0, 1))
+console.log(myInfoText.slice(286))
+
